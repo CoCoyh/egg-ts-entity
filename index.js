cdb4f1ddfa3201a62b7c4ee74161a25f0807ccea
guoyanhong <1352118502@qq.com>
2019-12-19 15:37:10 +0800

    init

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-'use strict';
+"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const { __decorate } = require('tslib');
-const { PrimaryGeneratedColumn, __metadata } = require('typeorm');
-
+const tslib_1 = require("tslib");
+const typeorm_1 = require("typeorm");
+// 实体类基类
 class BaseEntity {
 }
-
-__decorate([
-  PrimaryGeneratedColumn({ type: 'biginit' }),
-  __metadata("design:type", Number)
-], BaseEntity.prototype, 'id', void 0);
-__decorate([
-  Index(),
-  CreateDateColumn(),
-  __metadata("design:type", Date)
-], BaseEntity.prototype, 'createTime', void 0);
-__decorate([
-  UpdateDateColumn(),
-  __metadata("design:type", Date)
-], BaseEntity.prototype, 'updateTime', void 0);
-
+tslib_1.__decorate([
+    typeorm_1.PrimaryGeneratedColumn({ type: 'bigint' }),
+    tslib_1.__metadata("design:type", Number)
+], BaseEntity.prototype, "id", void 0);
+tslib_1.__decorate([
+    typeorm_1.Index(),
+    typeorm_1.CreateDateColumn(),
+    tslib_1.__metadata("design:type", Date)
+], BaseEntity.prototype, "createTime", void 0);
+tslib_1.__decorate([
+    typeorm_1.UpdateDateColumn(),
+    tslib_1.__metadata("design:type", Date)
+], BaseEntity.prototype, "updateTime", void 0);
 exports.BaseEntity = BaseEntity;
